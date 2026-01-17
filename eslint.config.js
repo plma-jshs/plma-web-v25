76571b59deca3c21ca1d15d104e953e1bbc0db92
mineppl12 <mineppl12@gmail.com>
2026-01-18 01:03:29 +0900

    feat: installed react-router, eslint, prettier

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js"
+import pluginReact from "eslint-plugin-react"
+import { defineConfig } from "eslint/config"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default defineConfig([
+    {
+        ignores: [
+            "**/node_modules/**",
+            "**/dist/**",
+            "**/build/**",
+            "**/coverage/**",
+            "**/+types/**",
+            "**/.react-router/**",
+        ],
+    },
+
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        languageOptions: { globals: { ...globals.browser } },
+        rules: {
+            "no-empty-pattern": "off",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+        settings: { react: { version: "detect" } },
+    },
+
+    pluginReact.configs.flat["jsx-runtime"],
+])
